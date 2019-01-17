6ef7dcecf7e9e176cb0c72ef36f69863f72c311e
Andy Ye <and1.w.ye@gmail.com>
2019-01-17 13:11:55 -0500

    about update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,19 +1,33 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 // Assets
 
 const styles = {
-  
+  grid: {
+    minHeight: '80vh',
+    width: '100%',
+  },
 };
 
 class About extends React.Component {
+
   render() {
+
+    const { classes } = this.props;
 
     return (
       <div>
-        
+        <Grid
+          className={classes.grid}
+          container
+          direction="column"
+          alignItems="center"
+          justify="center">
+        <Typography component="h2" variant="headline" gutterBottom><b>ABOUT ME</b></Typography>
+        </Grid>
       </div>
     )
   }
